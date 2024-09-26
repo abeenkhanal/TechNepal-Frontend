639b44d49207a675948e53a6ffab957728ac5cd1
Abeen Khanal <abeenkhanal16@gmail.com>
2024-09-26 07:38:55 +0545

    register page responsive done

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -4,151 +4,140 @@ import { useNavigate } from "react-router-dom";
 import { registerUser } from "../../../store/authSlice";
 import { STATUSES } from "../../../globals/components/misc/statuses";
 
-
 const Register = () => {
-    const dispatch = useDispatch()
-    const {status}= useSelector((state)=>state.auth)
-   
-  const navigate = useNavigate()
-  const [userData,setUserData] = useState({
-    username : "",
-    phoneNumber : "",
-    email : "",
-    password : ""
-    
-  })
-  const handleChange = (e)=>{
-    const {name,value} = e.target 
+  const dispatch = useDispatch();
+  const { status } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    username: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setUserData({
       ...userData,
-      [name] : value
-    })
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    dispatch(registerUser(userData))
-    if(status == STATUSES.SUCCESS){
-         navigate("/login")
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(registerUser(userData));
+    if (status === STATUSES.SUCCESS) {
+      navigate("/login");
     }
-    if(status == STATUSES.ERROR){
-        console.log("error")
-        return
+    if (status === STATUSES.ERROR) {
+      console.log("error");
+      return;
     }
-  }
+  };
 
   return (
-    <div>
-    <div className="max-w-full h-full w-full m-auto py-5 px-5 ml-44 relative group ">
-      <div className="container mx-auto">
-        <div className="flex justify-center items-center px-6 my-12">
-          <div className="w-full xl:w-3/4 lg:w-11/12 flex">
-            <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-              <h3 className="pt-4 text-2xl text-center">
-                Create an Account!
-              </h3>
-              <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={handleSubmit}>
-                
-                <div className="mb-4">
-                  <label className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="fullName"
-                  >
-                    Full Name
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="userName"
-                    name="userName"
-                    onChange={handleChange}
-                    type="text"
-                    required
-                    placeholder="Full Name"
-                  />
-                </div>
-
-
-                <div className="mb-4">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="email"
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    required
-                    placeholder="Email"
-                  />
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="phoneNumber"
-                  >
-                    Phone Number
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    onChange={handleChange}
-                    type="number"
-                    required
-                    placeholder="Phone Number"
-                  />
-                </div>
-
-              
-                  <div className="mb-4 md:mr-2 md:mb-0">
-                    <label
-                      className="block mb-2 text-sm font-bold text-gray-700"
-                      htmlFor="password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                      id="password"
-                      name="password"
-                      onChange={handleChange}
-                      type="password"
-                      placeholder="******************"
-                      required
-                    />
-                  </div>
-
-                <div className="mb-6 text-center">
-                  <button
-                    className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                  >
-                    Register Account
-                  </button>
-                </div>
-
-                 <hr className="mb-6 border-t" /> 
-
-                <div className="text-center">
-                  <a
-                    className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                    href="#"
-                    onClick={() => navigate("/login")}
-                  >
-                    Already have an account? Login!
-                  </a>
-                </div>
-              </form>
-            </div>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 p-4">
+      <div className="max-w-lg w-full bg-white rounded-lg shadow-lg p-6 space-y-6">
+        <h3 className="text-2xl font-semibold text-center text-gray-800">
+          Create an Account!
+        </h3>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label
+              className="block text-sm font-bold text-gray-700"
+              htmlFor="fullName"
+            >
+              Full Name
+            </label>
+            <input
+              className="w-full px-3 py-2 mt-2 text-sm text-gray-700 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              id="userName"
+              name="userName"
+              onChange={handleChange}
+              type="text"
+              required
+              placeholder="Full Name"
+            />
           </div>
-        </div>
+
+          <div>
+            <label
+              className="block text-sm font-bold text-gray-700"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="w-full px-3 py-2 mt-2 text-sm text-gray-700 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              type="email"
+              required
+              placeholder="Email"
+            />
+          </div>
+
+          <div>
+            <label
+              className="block text-sm font-bold text-gray-700"
+              htmlFor="phoneNumber"
+            >
+              Phone Number
+            </label>
+            <input
+              className="w-full px-3 py-2 mt-2 text-sm text-gray-700 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              id="phoneNumber"
+              name="phoneNumber"
+              onChange={handleChange}
+              type="number"
+              required
+              placeholder="Phone Number"
+            />
+          </div>
+
+          <div>
+            <label
+              className="block text-sm font-bold text-gray-700"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="w-full px-3 py-2 mt-2 text-sm text-gray-700 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              id="password"
+              name="password"
+              onChange={handleChange}
+              type="password"
+              required
+              placeholder="******************"
+            />
+          </div>
+
+          <div className="text-center">
+            <button
+              className="w-full px-4 py-2 font-bold text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
+            >
+              Register Account
+            </button>
+          </div>
+
+          <hr className="border-t" />
+
+          <div className="text-center">
+            <a
+              className="inline-block text-sm text-blue-600 hover:text-blue-800"
+              href="#"
+              onClick={() => navigate("/login")}
+            >
+              Already have an account? Login!
+            </a>
+          </div>
+        </form>
       </div>
     </div>
-  </div>
-);
-
+  );
 };
 
 export default Register;
